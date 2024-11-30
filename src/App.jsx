5b20eb72b309eb90def3200f8d0b3175b68ca2b6
lucaslinyker <lucaslinyker@outlook.com>
2024-11-30 12:58:29 -0300

    feat: criar componente App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,13 @@
+import { Header } from './components/Header.jsx';
+import { Main } from './components/Main.jsx';
+import { Footer } from './components/Footer.jsx';
+
 export function App() {
-  return <p>Hello World!</p>
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
 }
