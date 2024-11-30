8c195e030cb4e322476058624fd0f9dcf3d585a2
lucaslinyker <lucaslinyker@outlook.com>
2024-11-30 12:58:04 -0300

    feat: criar rodapé

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export function Footer() {
+  return (
+    <footer className='py-5 px-24 border-t'>
+      <p className='text-center'>
+        Criado e desenvolvido por{' '}
+        <a href='https://github.com/lucaslinyker' className='hover:underline'>
+          Lucas Linyker
+        </a>
+        <span className='font-black'>.</span>
+      </p>
+    </footer>
+  );
+}
